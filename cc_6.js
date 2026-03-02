13b303d91dea18e3441b6ebcf3ecdeeb412207c0
Shashwat <ss44@usf.edu>
2026-03-01 20:59:00 -0500

    implement company workflow

--- cc_6.js
+++ cc_6.js
@@ -1,39 +1,36 @@
 //step 2
-class employee {
+class Employee {
     constructor(name, department) {
         this.name = name;
         this.department = department;    
 }
 describe() {
-    return `${this.name} works in the ${this.department} department.`;
+    return `${this.name} works in the ${this.department} department`;
 }
 }
 
 //step 3
-class manager extends employee {
+class Manager extends Employee {
     constructor(name, department, teamSize) {
         super(name, department);
         this.teamSize = teamSize;
     }
     describe() {
-        return `${super.describe()} and manages a team of ${this.teamSize} employees in the ${this.department} department.`;
+        return `${super.describe()} and manages a team of ${this.teamSize}.`;
     }
 }
 
 //step 4
-const emp1 = new employee("Aiden", "Finance");
-console.log(emp1.describe());
-const emp2 = new employee("Alex", "Marketing");
-console.log(emp2.describe());
+const emp1 = new Employee("Aiden", "Finance");
+const emp2 = new Employee("Alex", "Marketing");
+const mgr1 = new Manager("Edo", "Engineering", 5);
+const mgr2 = new Manager("Rj", "Finance", 3);
 
-const mgr1 = new manager("Edo", "Engineering", 5);
-console.log(mgr1.describe());
-const mgr2 = new manager("Rj", "Finance", 3);
-console.log(mgr2.describe());
 
 //step 5
-class company {
+class Company {
     constructor(name) {
+        this.name = name;
         this.employees = [];
     }
     addEmployee(employee) {
@@ -45,3 +42,12 @@ class company {
     }
 }
 }
+
+//step 6
+const company = new Company("Shash's Company");
+company.addEmployee(emp1);
+company.addEmployee(emp2);
+company.addEmployee(mgr1);
+company.addEmployee(mgr2);
+
+company.listEmployees();
