e2b5562988af339f38ae95644443a9d9442546fe
Shashwat <ss44@usf.edu>
2026-03-01 19:19:21 -0500

    made base class

---
+++ cc_6.js
@@ -0,0 +1,13 @@
+//step 2
+class employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;    
+}
+describe() {
+    return `${this.name} works in the ${this.department} department.`;
+}
+}
+
+//step 3
+
